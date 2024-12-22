a38a13149a9b2c887a2aa4bb87de66b235b4a015
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-22 20:02:39 +0700

    memperbaiki hubungan antar tabel

--- app/src/main/java/com/example/ucp_2_b/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucp_2_b/data/database/TokoDatabase.kt
@@ -9,7 +9,7 @@ import com.example.ucp_2_b.data.dao.SuplierDao
 import com.example.ucp_2_b.data.entity.Barang
 import com.example.ucp_2_b.data.entity.Suplier
 
-@Database(entities = [Barang::class],[Suplier::class], version = 1, exportSchema = false)
+@Database(entities = [Barang::class , Suplier::class], version = 1, exportSchema = false)
 abstract class TokoDatabase : RoomDatabase() {
 
     abstract fun barangDao(): BarangDao
